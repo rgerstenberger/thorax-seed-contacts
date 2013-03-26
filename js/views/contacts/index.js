345dc212ef9a4d00cca9d949aa570347d31db425
Rob Gerstenberger <rgerstenberger@gmail.com>
2013-03-25 17:09:01 -0700

    Simplified index view events and template
    Index now only has an event for delete. Removed form and edit buttons
    from index template since they are redundant

--- js/views/contacts/index.js
+++ js/views/contacts/index.js
@@ -1,45 +1,12 @@
 Application.View.extend({
   name: "contacts/index",
   events: {
-  	'submit form[name="add"]': function(){
-  		event.preventDefault();
-  		var attrs = this.serialize();
-  		this.collection.add(attrs);
-      this.collection.last().set({id: Application.idPlaceholder});
-      Application.idPlaceholder++;
-  		this.$('input[type!="submit"]').val('');
-  	},
-    'submit form[name="edit"]': function(){
-      event.preventDefault();
-      var attrs = this.serialize();
-      var id = this.$('form').data("model-cid");
-      var item = this.collection.get(id);
-      item.set(attrs);
-      this.$('input[type!="submit"]').val('');
-      this.$('form').attr("name","add");
-      this.$('input[type="submit"]').val('add');
-    },
   	"click a.delete": function(){
   		event.preventDefault();
   		var id = $(event.srcElement).parent().data("model-cid");
         var item = this.collection.get(id);
   		this.collection.remove(item);
-  	},
-  "click a.edit": function(){
-        event.preventDefault();
-        var id = $(event.srcElement).parent().data("model-cid");
-        var attrs = this.collection.get(id).attributes;
-        this.$('form').attr("name", "edit");
-        this.$('form').data("model-cid", id);
-        this.$('input[type="submit"]').val('update');
-        for( var attribute in attrs){
-          this.$('input[name="' + attribute + '"]').val(attrs[attribute]);
-        }
-        
-      },
-
-
-
+  	}
   }
 });
 
